a29cc9ae44bf43db273c91cd1f8acdd0991f472a
Nur Sasongko <kokonur1020@gmail.com>
2024-08-10 18:06:27 +0700

    feat: add Wikipedia functionality to the legend of python wiki.py

---
+++ the-legend-of-python/42-forty-two/wiki.py
@@ -0,0 +1,8 @@
+import wikipedia
+print(f"{wikipedia.summary('Wikipedia')}\n")
+
+print(f"{wikipedia.search('Philosophy of life')}\n")
+
+print(f"{wikipedia.search('Python')}\n")
+
+print(f"{wikipedia.search('Galaxy')}\n")
